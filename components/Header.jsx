5019fb6e172503ddd29d7a907814c88b242919bc
Hanna Shymanovych <shimsnov111@gmail.com>
2025-04-06 00:07:04 +0200

    Header done

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <img src="images/chef-claude-icon.png" alt="a face of a chef robot" />
+                <a href="#">Chef Claude</a>
+            </nav>
+        </header>
+    )
+}
