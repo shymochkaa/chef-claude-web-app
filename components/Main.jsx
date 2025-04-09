a9f0d5500bfb2f8bc62f10bde4cf450d51a507ba
Hanna Shymanovych <shimsnov111@gmail.com>
2025-04-09 21:54:14 +0200

    Ingredients challenge

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,10 +1,24 @@
 export default function Main() {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    
+    
+    const listOfIngredients = ingredients.map((ingredient) => {
+        return <li>{ingredient}</li>
+    } )
 
     return(
-       <form action="#">
+        <main>
+            <form action="#">
             <input type="text" placeholder="e.g. oregano" aria-label='Type ingredient' />
-            <button aria-label="Submit" >Add ingredient</button>
+            <button>Add ingredient</button>
        </form>
+        <ul>
+            {listOfIngredients}
+        </ul>
+
+        </main>
+       
     )
    
 }
+
