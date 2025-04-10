454e770c46be75d43bf27e5e536f82b1d5ce2adc
Hanna Shymanovych <shimsnov111@gmail.com>
2025-04-10 18:19:15 +0200

    Update Main.jsx - Form done

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,15 +1,31 @@
+
+import {useState} from 'react'
 export default function Main() {
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const [ingredients, setIngredients] = useState(["Chicken", "Oregano", "Tomatoes"]) 
     
     
+
+
     const listOfIngredients = ingredients.map((ingredient) => {
         return <li key={ingredient}>{ingredient}</li>
     } )
 
+    function handleSubmit(e){
+        e.preventDefault()
+        const form = document.querySelector("#form")
+        const formData = new FormData(form)
+        const newIngredient = formData.get("ingredient")
+        
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+
+
+    }
+
+
     return(
         <main>
-            <form action="#">
-            <input type="text" placeholder="e.g. oregano" aria-label='Type ingredient' />
+            <form action="#" id="form" onSubmit={handleSubmit}>
+            <input type="text" placeholder="e.g. oregano" aria-label='Type ingredient' name="ingredient" />
             <button>Add ingredient</button>
        </form>
         <ul>
